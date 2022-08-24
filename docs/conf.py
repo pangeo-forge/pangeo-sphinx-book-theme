dfb8af7c10d177ef0e9098a5d6d9be69c35b087c
Chris Holdgraf <choldgraf@berkeley.edu>
2022-08-24 05:07:51 +0200

    Add demo documentation site and unpin book theme

---
+++ docs/conf.py
@@ -0,0 +1,6 @@
+project = "Pangeo Book Theme"
+html_title = "Pangeo Book Theme"
+html_theme = "pangeo_sphinx_book_theme"
+extensions = ["sphinx.ext.autodoc"]
+html_logo = "_static/pangeo-forge-logo-blue.png"
+
---
+++ docs/index.rst
@@ -0,0 +1,9 @@
+Test documentation
+==================
+
+This is simple documentation to test the Pangeo Book Theme.
+
+.. toctree::
+   :glob:
+
+   kitchen-sink/*
